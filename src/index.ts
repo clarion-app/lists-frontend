170bbdcd3ba65fb432efbda10a750e661caa1ce4
Tim Schwartz <tim@metaverse.systems>
2024-08-19 15:39:20 -0500

    chore: Bump package version to 1.0.14 and update listsApi in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { BackendType } from "@clarion-app/types";
-import { listsApi as listsFrontendApi } from "./listsApi";
+import { listsApi } from "./listsApi";
 import Lists from "./Lists";
 import List from "./List";
 
 export const backend: BackendType = { url: "http://localhost:8000", token: "" };
 
-const initializeListsFrontend = (setBackendUrl: string) => {
+const initializeFrontend = (setBackendUrl: string) => {
     backend.url = setBackendUrl;
 };
 
-const setListsFrontendToken = (token: string) => {
+const setFrontendToken = (token: string) => {
     backend.token = token;
 };
 
-export { initializeListsFrontend, listsFrontendApi, setListsFrontendToken, Lists, List };
+export { initializeFrontend, listsApi, setFrontendToken, Lists, List };
