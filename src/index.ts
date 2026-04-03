dd0f323e802482d52e23b2adffcf422466522b3f
Tim Schwartz <tim@metaverse.systems>
2026-04-03 06:25:01 -0500

    chore: update @clarion-app/types dependency and remove token handling from backend

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import { listsApi } from "./listsApi";
 import Lists from "./Lists";
 import List from "./List";
 
-export const backend: BackendType = { url: "http://localhost:8000", token: "", user: { id: "", name: "", email: ""} };
+export const backend: BackendType = { url: "http://localhost:8000", user: { id: "", name: "", email: ""} };
 
 export const updateFrontend = (config: BackendType) => {
     backend.url = config.url;
-    backend.token = config.token;
     backend.user = config.user;
 };
 
